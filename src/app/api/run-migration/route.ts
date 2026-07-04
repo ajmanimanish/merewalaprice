f1820041936822a6ebb43e1bdfe0a1e5cb03825a
Manish Ajmani <ajmani.manish@gmail.com>
2026-07-04 20:25:50 +0530

    chore: remove square brackets from IPv6 host

--- src/app/api/run-migration/route.ts
+++ src/app/api/run-migration/route.ts
@@ -55,7 +55,7 @@ export async function GET(request: Request) {
   }
 
   const passwords = ['bhopalprice123', 'MWP@Admin2024'];
-  const host = '[2406:da12:1f1:f802:77fd:3ed:b6cb:b46d]';
+  const host = '2406:da12:1f1:f802:77fd:3ed:b6cb:b46d';
   const user = 'postgres';
   const database = 'postgres';
   const port = 5432;
